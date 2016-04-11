4a5ac1c95179da4e64a890abeebcfc00159c7b6c
XProbie <XProbie@gmail.com>
2016-04-11 10:12:26 +0800

    add README.md

--- multithreading/base.py
+++ multithreading/base.py
@@ -5,7 +5,12 @@
 'Some methods about multithreading in Python'
 
 __author__ = 'AJ Kipper'
-
+'''
+thread is the underlying support module in python,and threading is the
+encapsulation of thread,so it's better to use threading instead of thread
+'''
 import threading
+
+
 
 
