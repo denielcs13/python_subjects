c1a940cd9ca439a2b031c04f4cbd21d89a540c17
XProbie <XProbie@gmail.com>
2016-04-11 09:55:28 +0800

    update

---
+++ decorator/func.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+#python 2.7.6
+
+'''
+Know about functions in Python
+functions are 'first-class' citizens in Python,meaning they can be assigned to variables, passed to other functions, etc., just like other values
+'''
+
+__author__ = 'AJ Kipper'
+
+#Assigned to variables
+def foo(name):
+    return 'Hello,' + name
+
+#You can just direct call
+print foo('AJ Kipper')
+#Output: Hello,AJ Kipper!
+
+#Or you can assigned to variables
+greet = foo
+print greet('AJ Kipper')
+#Output: Hello,World!
+
+#Or as a parameter to another function
+def morning(func):
+    return func + ',morning'
+print morning(greet('AJ Kipper'))
+
